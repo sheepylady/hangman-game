ca3fd39bfd442962d8b006556fdae890b58b8710
sheepylady <debbie.hamp@gmx.co.uk>
2023-12-14 15:13:42 +0000

    ##milestone_4.py
    this file is a possible example of oop programming. A class Hangman is created and contains everything for playing a round of hangman. In this class the following variables are intialised.
    the following variables are definded
    the word_list and number _of Lives(which is set to 5) are passed as arguemnts and are set as self. i.e. self.word_list and self_num_of_lives.
    the word is chosen using the random.choice function and called self.word.
    the list word_guessed is initalised using '' to represent each letter. The list is as long as the word.
    the num_letters is the number of UNIQUE letters in the word using (list(len(set.self.word)).
    an empty list called list_of _guesses.
    
    the check_guess function is expanded from mileston_3.py.
     a for loop is added in the if statement after the print statement. the for loop looks at each letter(variable each_letter) and the posistion(variable count) it holds in the word. it then compares it to the guess and if it is the same then places the letter in the list word_guessed at the index count. Outside the if statement containing the for loop  but within the  outside if statement the num_letters is reduced by 1.
     The else statement is also changed.
     num_lives is reduced by 1
     an additional print statement is added to let the user know that there are a number of lives left.

---
+++ milestone_4.py
@@ -0,0 +1,78 @@
+import random
+
+class Hangman():
+ '''This class implements the game Hangman. 
+ 
+     It has 3 methods. The first is a magic method to initalise incoming arguements
+     of word_list and the num_of_lives which is set to 5. Additional variables 
+     that are intialised here are  
+     word - the random choice from the word_list
+     word_guessed - the list filled with spaces for characters. It is the length of the word.
+     num_letters - which is number of unique letters in the chosen word
+     finally list_of guesses - initalised as an empty list but it is used to hold all the quesses 
+     of the user so that they can be told if they have tried that letter before
+     The 2nd method the  ask_for input which has a while loop set to true. in it 
+     is an input statement which asks the user to input a single letter. 
+     there is an if statement to check that the input is a single letter. if not 
+     a print asttement is executed telling the user to try again. this is followed by elif 
+     which tells the user that they have tried this letter before (because the letter is in the 
+     list_of_guesses list.) Finally if the letter is single and a character and not in the list of guesses then
+     it calls the final function check_guess.
+     check_guess takes the letter and sees if it is in the word. if it is then it looks to see where it is
+     in the word and places the letter in the appropriate place in the word_guessed list, replacing one of the empty slots
+     It also reduces the number of letters to be found by. IF the letter is not in the word the number of lives is 
+     reduced by 1, 2 print statements are issued to say that the letter was not in the word and they have so many lives left.
+     The guess is placed in the list of guesses
+           '''
+    
+ def __init__(self,word_list,num_lives = 5):
+    self.word_list = word_list #list of words to choose from
+    self.num_lives = num_lives #number of tries
+    self.word = random.choice(self.word_list) #random word chosen from word_list
+    self.word_guessed = [''] * len(self.word)  #list of empty spaces =to length of the word
+    self.num_letters = len(list(set(self.word))) # number of unique chrs in word
+    self.list_of_guesses = [] #list of guesses
+      
+    print(f"The mystery word has {self.num_letters} characters")
+         
+ def ask_for_input(self):
+  while True:
+    guess = input ("Please choose a single letter ")
+   
+    if not (len(guess) == 1 and guess.isalpha()):
+         print( "Invalid letter. Please, enter a single alphabetical character.")
+    elif guess in self.list_of_guesses: 
+        print( "You already tried that letter!") 
+    else:
+       print(guess) 
+       self.check_guess(guess) # function to see if letter is part of the chosen word
+     
+
+ def check_guess(self,guess):
+  guess.lower() 
+  if guess in self.word:
+        print(f"Good guess! {guess} is in the word.")    
+        for count, each_letter in enumerate(self.word):
+            if  guess == each_letter:
+                self.word_guessed[count]= guess
+
+        self.num_letters = self.num_letters -1           
+  else:
+        self.num_lives = self.num_lives -1
+        print(f"Sorry, {guess} is not in the word. Try again.")
+        print(f"You have {self.num_lives} lives left.")
+        self.list_of_guesses.append(guess) 
+        print(self.list_of_guesses)   
+
+
+
+
+#word list
+
+word_list = ['cherry', 'lychee', 'apricot', 'apple','kiwi']
+
+
+
+#set the game off
+
+Hangman(word_list).ask_for_input()
